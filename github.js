5bd98b946280d1318bbf638ed45bba3570187dae
Jeff Israel <jeff.israel@gmail.com>
2017-03-18 10:41:15 -0500

    Simple github api client to create and list issues for a repo

---
+++ github.js
@@ -0,0 +1,42 @@
+const reqPromise = require('request-promise');
+const urlJoin = require('url-join');
+
+class GitHub {
+  constructor(token) {
+    this.apiUrl = 'https://api.github.com';
+    this.headers = {
+      Authorization: `token ${token}`,
+      'User-Agent': 'stripethree'
+    };
+  }
+
+  createIssue(owner, repo, title, body) {
+    const reqBody = {
+      title,
+      body,
+      assignees: [owner]
+      // milstone,
+      // labels: []
+    }
+    const options = {
+      body: reqBody,
+      headers: this.headers,
+      method: 'POST',
+      json: true,
+      uri: urlJoin(this.apiUrl, 'repos', owner, repo, 'issues')
+    }
+    return reqPromise.post(options);
+  }
+
+  listIssues(owner, repo) {
+    const options = {
+      headers: this.headers,
+      method: 'GET',
+      json: true,
+      uri: urlJoin(this.apiUrl, 'repos', owner, repo, 'issues')
+    }
+    return reqPromise.get(options);
+  }
+}
+
+exports.GitHub = GitHub
