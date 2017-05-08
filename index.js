8b5bb9a3d05a1ca79b5b8b22e508f298626a30da
Jeff Israel <jeff.israel@gmail.com>
2017-05-08 16:36:26 -0500

    Session example - simple message history

--- index.js
+++ index.js
@@ -25,7 +25,22 @@ messenger.on('text.help', ({reply}) => {
   reply(new responses.Text('🤖  I am here to offer my services to assist you.'));
 });
 
-messenger.on('text', ({reply, senderId, text, source}) => {
+messenger.on('text', ({reply, senderId, session, text, source}) => {
+
+  // using the sessiono object - simple message history
+  /*
+  const matchesLast = text.match(/^last (\d)$/i);
+  if (matchesLast) {
+    if (session.messages && session.messages.length) {
+      const history = session.messages.slice(parseInt(matchesLast[1], 10)*-1).reverse().map((message, index) => `${index+1}. ${message}`).join('\n');
+      reply(new responses.Text('historyReply', matchesLast[1], history));
+    } else {
+      reply(new responses.Text('noHistoryReply'));
+    }
+    return;
+  }
+  session.messages ? session.messages.push(text) : session.messages = [text];
+  */
   reply(new responses.Text(`Echo: "${text}"`));
 });
 
--- messages.js
+++ messages.js
@@ -2,5 +2,7 @@
 module.exports = {
   greetingReply1: '🤖 beep bop boop. Well hello there, I am a bot.',
   greetingReply2: '🤖 beep bop boop. Well hello there, %s, I am a bot.',
-  helpReply: '🤖 Ask me a question, and I open a GitHub issue for you.'
+  helpReply: '🤖 Ask me a question, and I open a GitHub issue for you.',
+  historyReply: 'Here are your last %s messages:\n%s',
+  noHistoryReply: 'You have not sent us any messages yet!'
 };
